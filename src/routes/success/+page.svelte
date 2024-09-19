9da6f532ec99d595e74edb5e0e68ca439ba2fb76
adityaoberai <adityaoberai1@gmail.com>
2024-09-20 01:27:37 +0530

    Add cloud credits button

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -10,5 +10,6 @@
 <div class="container u-flex-vertical u-gap-32">
 	<h1 class="heading-level-1 u-normal">Success!</h1>
 	<p class="heading-level-6 u-normal u-text-center">Thanks for signing up in the hackathon, {userId}</p>
-	<a class="button is-big" href="https://github.com/appwrite-community/htf24-hackathon-submissions" target="_blank">Create submission</a>
+	<a class="button is-big" href="https://github.com/appwrite-community/htf24-hackathon-submissions" target="_blank">Submit your hackathon project</a>
+	<a class="button is-big" href="https://apwr.dev/hacktoberfest" target="_blank">Claim Appwrite Cloud credits</a>
 </div>
