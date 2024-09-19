117e679c49bea9050cea1369b2ac52798ea4741d
adityaoberai <adityaoberai1@gmail.com>
2024-09-19 22:10:23 +0530

    Update logging

--- src/routes/success/+server.js
+++ src/routes/success/+server.js
@@ -4,8 +4,9 @@ import { GITHUB_PERSONAL_ACCESS_TOKEN } from '$env/static/private';
 export async function POST({ request }) {
     try {
         const requestBody = await request.json();
+        const githubUsername = requestBody.github;
 
-        const uri = `https://api.github.com/repos/appwrite-community/htf24-hackathon-submissions/collaborators/${requestBody.github}`;
+        const uri = `https://api.github.com/repos/appwrite-community/htf24-hackathon-submissions/collaborators/${githubUsername}`;
 
         const headers = {
             'Accept': 'application/vnd.github+json',
@@ -18,7 +19,9 @@ export async function POST({ request }) {
             headers: headers
         });
 
-        return new Response(JSON.stringify({ success: true }), { status: response.status });
+        console.log(`${githubUsername} added as a collaborator to the repository\n\nResponse status: ${response.status}`);
+
+        return new Response(JSON.stringify({ success: true }), { status: 200 });
     } catch(error) {
         console.error(error);
         return new Response(JSON.stringify({ success: false, error: error }), { status: 500 });
