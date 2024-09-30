e8277802bc3d5f03f9e791de3ef563c5180b4f25
adityaoberai <adityaoberai1@gmail.com>
2024-09-30 20:33:21 +0530

    Remove $id from userlist function

--- functions/userlist/src/main.js
+++ functions/userlist/src/main.js
@@ -2,7 +2,6 @@ export default async ({ req, res, log, error }) => {
   const requestBody = req.bodyJson;
   const email = requestBody.email;
   const name = requestBody.name != '' ? requestBody.name : email;
-  const userId = requestBody.$id;
 
   try {
     let userlistResponse = await fetch(
@@ -15,20 +14,19 @@ export default async ({ req, res, log, error }) => {
         body: JSON.stringify({
           email,
           name,
-          userId,
         }),
       }
     );
     let response = await userlistResponse.json();
 
     if (response.success == true) {
-      log(`User ${userId} added to UserList`);
+      log(`User ${email} added to UserList`);
       return res.empty();
     } else {
       throw new Error(`Growth server error`);
     }
   } catch (err) {
-    error(`Failed to add user ${userId} to UserList\n\nError details: ${err}`);
+    error(`Failed to add user ${email} to UserList\n\nError details: ${err}`);
     return res.empty();
   }
 };
